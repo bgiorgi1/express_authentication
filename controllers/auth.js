037ea4981dfb7fa27ceb35ed76f8abc5957977b2
“bgiorgi1” <brianna.giorgi@gmail.com>
2021-05-14 10:42:51 -0400

    alerts: add partials for flash alerts

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,7 +23,7 @@ router.get('/logout', (req, res) => {
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/auth/login',
-  successFlash: 'Welcome back ...',
+  successFlash: 'Welcome back ...', //render when logged in
   failureFlash: 'Either email or password is incorrect' 
 }));
 
